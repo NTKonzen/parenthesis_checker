7140fc0a4f45b41738794f2635b18c0771048cbf
Nicholas Konzen <nicholaskonzen@gmail.com>
2024-09-05 12:49:16 -0700

    fix node version

--- nodeJsVersion.js
+++ nodeJsVersion.js
@@ -1,19 +1,21 @@
 const str = process.argv[2]
 
 let valid = !!str
-const map = {
-  "}": "{",
-  "]": "[",
-  ")": "("
-}
-const stackArr = []
-str.split("").forEach(char => {
-  if (map[char]) {
-    if (stackArr.pop() !== map[char]) {
-      valid = false
-    }
+
+if (valid) {
+  const map = {
+    "}": "{",
+    "]": "[",
+    ")": "("
   }
-  stackArr.push(char)
-})
+  const stackArr = []
+  str.split("").forEach(char => {
+    if (map[char]) {
+      if (stackArr.pop() !== map[char]) {
+        valid = false
+      }
+    } else stackArr.push(char)
+  })
+}
 
 console.log(valid ? "Valid" : "Invalid")
