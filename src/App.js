258550bf6001c1061b434213ca00b263055aa533
Nicholas Konzen <nicholaskonzen@gmail.com>
2024-09-05 00:22:17 -0700

    quick n' ugly one-page solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const checkValidity = str => {
+  let valid = !!str
+  const map = {
+    "}": "{",
+    "]": "[",
+    ")": "("
+  }
+  const stackArr = []
+  str.split("").forEach(char => {
+    if (map[char]) {
+      if (stackArr.pop() !== map[char]) {
+        valid = false
+      }
+    } else stackArr.push(char)
+  })
+
+  return !stackArr.length && valid
 }
 
-export default App;
+function App() {
+  const [input, setInput] = useState("")
+  const [isValid, setIsValid] = useState(true)
+  const [output, setOutput] = useState([])
+
+  const handleInput = ({ currentTarget: { value } }) => {
+    setInput(value)
+    setIsValid(checkValidity(value))
+  }
+
+  const handleSubmit = () => {
+    if (isValid && input) {
+      setOutput(prev => [...prev, input])
+      setInput("")
+    }
+  }
+
+  const handleKeyPress = ({ key }) => {
+    if (key === "Enter") {
+      handleSubmit()
+    }
+  }
+
+  return (
+    <div className='wrapper'>
+      <div className='card'>
+        <div className='inputBox' onKeyDown={handleKeyPress}>
+          <input
+            value={input}
+            onChange={handleInput}
+            className={isValid ? "valid" : "invalid"}
+          />
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+        <div>{isValid ? "Valid" : "Invalid"}</div>
+        <div className='outputBox'>
+          {output.map((item, index) => (
+            <span key={index}>{item}</span>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
